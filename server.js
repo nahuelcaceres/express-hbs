21f90ae7663590d78b5e3b5aeba92cc6ac5b4848
nahuel <nahuecaceres@gmail.com>
2018-11-16 16:42:55 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs'); 
+
+require('./hbs/helpers');
+
+const port = process.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+// Express hbs engine
+app.set('view engine', 'hbs');
+
+
+app.get('/', function (req, res) {
+
+    res.render('home',{
+        nombre: 'nahuel matias'
+    });
+
+});
+
+app.get('/about', function(req, res){
+
+    res.render('about');
+
+});
+
+app.get('/data', (req, res) =>{
+
+    res.send('Hola data');
+
+});
+
+app.listen(port,() =>{
+    console.log(`Servidor escuchando en el puerto ${ port }`);
+});
